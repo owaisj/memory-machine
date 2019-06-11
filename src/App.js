6b548023beb9e6c7445ea653b5d0ef97ea0db656
owaisj <jamil.owais@gmail.com>
2019-06-11 15:41:25 -0500

    Imported app components to display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from './Components/Navbar'
+import Board from './Components/Board'
 
 function App() {
   return (
     <div>
-      <h1>
-        Memory Machine
-      </h1>
+      <Navbar />
+      <Board />
     </div>
   );
 }
