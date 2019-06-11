8209d8b651b8fe6b142593d7b832e17caac3f6ca
owaisj <jamil.owais@gmail.com>
2019-06-11 16:44:19 -0500

    onClick for each mapped image

--- src/Components/Piece.js
+++ src/Components/Piece.js
@@ -2,13 +2,21 @@ import React, {Component} from 'react'
 
 export default class Piece extends Component {
     render() {
+        const data = this.props.data;
         return (
-            <div>
-                <img 
-                    src="http://www.fillmurray.com/200/200" 
-                    alt="fillmurray 200-200"
-                    />
-            </div>
+            data.map((item, i) => {
+                return (
+                    <div key={i} onClick={() => {
+                        if (data[i].isClicked === true) return alert('Clicked already!')
+                        data[i].isClicked = true;
+                        }
+                    }>
+                        <img src={item.url} 
+                            alt="fillmurray placeholder"
+                        />
+                    </div>
+                )
+            })
         )
     }
 }
