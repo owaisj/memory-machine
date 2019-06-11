6f65959a6a882ef655360425d0f7c9aca1168f79
owaisj <jamil.owais@gmail.com>
2019-06-11 16:43:27 -0500

    Styles and images array as props

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,19 +3,31 @@ import Piece from './Piece'
 const styles = {
     container: {
         border: '2px solid #b98ea7',
+        backgroundColor: '#f5f2d0',
         minHeight: '200px',
         marginTop: '1em',
         display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-betwee'
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: '1em'
     }
 }
+
+const images = [
+    //TODO: Add more 200x200 images
+    {
+        url: '/assets/images/murray1.jpg'
+    },
+    {
+        url: '/assets/images/murray2.jpg'
+    }
+]
 
 export default class Board extends Component {
     render() {
         return (
             <div style={styles.container}>
-                <Piece />
+                <Piece data={images}/>
             </div>
         )  
     }
