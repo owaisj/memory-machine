c21a23423bf01bcf3d6c55994b852a5e0541969e
owaisj <jamil.owais@gmail.com>
2019-06-11 23:36:26 -0500

    Score updating and reset with click handling

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -13,21 +13,65 @@ const styles = {
     }
 }
 
-const images = [
+let images = [
     //TODO: Add more 200x200 images
     {
+        id: 1,
         url: '/assets/images/murray1.jpg'
     },
     {
+        id: 2,
         url: '/assets/images/murray2.jpg'
+    },
+    {
+        id: 3,
+        url: 'assets/images/murray3.jpg'
+    },
+    {
+        id: 4,
+        url: 'assets/images/murray4.jpg'
     }
 ]
 
 export default class Board extends Component {
+    constructor() {
+        super();
+        this.state = {
+            score: 0,
+            clicked: [],
+            highScore: 0
+        }
+        this.handleClick = this.handleClick.bind(this);
+    };
+    reset() {
+        this.setState({
+            score: 0,
+            clicked: []
+        }, this.props.grabScore(this.state.highScore, 0))
+    }
+    handleClick(id) {
+        let highScore = this.state.highScore;
+        //Correct Guess
+        if (this.state.clicked.indexOf(id) === -1) {
+            let currentScore = this.state.score + 1;
+            //Check for new high score
+            if (currentScore > highScore) highScore = currentScore;
+            this.setState({
+                highScore: highScore,
+                score: currentScore,
+                clicked: this.state.clicked.concat(id)
+            }, this.props.grabScore(highScore, currentScore))
+        //Incorrect Guess
+        } else {
+            this.reset();        
+        }
+    }
     render() {
         return (
             <div style={styles.container}>
-                <Piece data={images}/>
+                <Piece 
+                    handleClick={this.handleClick}
+                    data={images}/>
             </div>
         )  
     }
