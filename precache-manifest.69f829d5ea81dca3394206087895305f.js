09b2088f621841c46c8f8ec121e557a9673f3281
owaisj <jamil.owais@gmail.com>
2019-06-12 00:09:20 -0500

    Updates

---
+++ precache-manifest.69f829d5ea81dca3394206087895305f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5294b7723314b069b66ec162420530e5",
+    "url": "/memory-machine/index.html"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/memory-machine/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "fed550af45dd190741a4",
+    "url": "/memory-machine/static/js/main.4df86edf.chunk.js"
+  },
+  {
+    "revision": "2f040e9cc6b7a17e0f0f",
+    "url": "/memory-machine/static/js/runtime~main.5857f4c1.js"
+  }
+]);
