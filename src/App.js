9a9889b965b82da214177ada919fe2112d01f8c7
owaisj <jamil.owais@gmail.com>
2019-06-11 23:35:37 -0500

    Class based component with score as state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from './Components/Navbar'
 import Board from './Components/Board'
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Board />
-    </div>
-  );
+class  App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      highScore: 0,
+      score: 0
+    }
+    this.handleChange = this.handleChange.bind(this)
+  };
+  handleChange(top, cur) {
+    this.setState({
+      highScore: top,
+      score: cur
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Navbar top={this.state.highScore}
+          current={this.state.score}
+        />
+        <Board 
+          grabScore={this.handleChange}
+        />
+      </div>
+    )
+  };
 }
 
 export default App;
